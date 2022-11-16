ab66a2e98160b80329d0dd917960baf010c943cf
AlexanderDmitriev <alex9110077@gmail.com>
2022-11-16 22:22:07 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import JobList from './JobList';
 import DetailedJob from './DetailedJob/DetaliedJob';
 import { Container, Wrapper } from './Container';
+import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
     <Wrapper>
       <Container>
+        {/* <Routes>
+          <Route></Route>
+          <Route></Route>
+        </Routes> */}
         <JobList />
-        <DetailedJob/>
+        <DetailedJob />
       </Container>
     </Wrapper>
   );
